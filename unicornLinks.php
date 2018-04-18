24dd24152687323b385ac20833e88aa231c1feda
ShervinMansouri <shervin05@hotmail.com>
2018-04-18 14:24:13 +0200

    Commit text

---
+++ unicornLinks.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>List of unicorns</title>
+    <link rel='stylesheet' href='/static/style.css' />
+    <link href='http://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  </head>
+  <body>
+    <h1>The fantastic unicorn database</h1>
+    <ul>
+        <li class="unicorn" id="unicorn1">
+        <a href="http://unicorns.idioti.se/1">Nordsvensk travhörning</a>
+      </li>
+        <li class="unicorn" id="unicorn2">
+        <a href="http://unicorns.idioti.se/2">Karibisk strandponny</a>
+      </li>
+        <li class="unicorn" id="unicorn3">
+        <a href="http://unicorns.idioti.se/3">Nattaktiv hornlöpare</a>
+      </li>
+        <li class="unicorn" id="unicorn4">
+        <a href="http://unicorns.idioti.se/4">Tordönsenhörning</a>
+      </li>
+        <li class="unicorn" id="unicorn5">
+        <a href="http://unicorns.idioti.se/5">Indisk enhörning</a>
+      </li>
+        <li class="unicorn" id="unicorn6">
+        <a href="http://unicorns.idioti.se/6">Småväxt enhörning</a>
+      </li>
+        <li class="unicorn" id="unicorn7">
+        <a href="http://unicorns.idioti.se/7">Transsylvansk kortsvans</a>
+      </li>
+        <li class="unicorn" id="unicorn8">
+        <a href="http://unicorns.idioti.se/8">Grekisk pegasusmix</a>
+      </li>
+        <li class="unicorn" id="unicorn9">
+        <a href="http://unicorns.idioti.se/9">Eventuellt fejkad enhörning</a>
+      </li>
+        <li class="unicorn" id="unicorn10">
+        <a href="http://unicorns.idioti.se/10">Bayersk lättfotad</a>
+      </li>
+        <li class="unicorn" id="unicorn11">
+        <a href="http://unicorns.idioti.se/11">Fransk vågman</a>
+      </li>
+        <li class="unicorn" id="unicorn12">
+        <a href="http://unicorns.idioti.se/12">Mulfläckad enhörning</a>
+      </li>
+        <li class="unicorn" id="unicorn13">
+        <a href="http://unicorns.idioti.se/13"></a>
+      </li>
+        <li class="unicorn" id="unicorn14">
+        <a href="http://unicorns.idioti.se/14"></a>
+      </li>
+        <li class="unicorn" id="unicorn15">
+        <a href="http://unicorns.idioti.se/15"></a>
+      </li>
+        <li class="unicorn" id="unicorn16">
+        <a href="http://unicorns.idioti.se/16"></a>
+      </li>
+       </ul>
+  </body>
+</html>
